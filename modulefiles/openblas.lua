bf39dadd3c79bdb6b8e129873b66882431c382f3
Paul T. Bauman <ptbauman@gmail.com>
2015-02-14 16:18:09 -0500

    Tweaks to OpenBLAS modulefile

--- modulefiles/openblas.lua
+++ modulefiles/openblas.lua
@@ -1,6 +1,11 @@
 
 family("openblas")                                                                                                                                                                                                                           
 
+local hier = hierarchyA("openblas", 5)
+
+local compiler = hier[5]
+local compiler_version = hier[4]
+
 help(
 "\n"..
 "This module loads OpenBLAS and sets:"..
@@ -13,7 +18,7 @@ help(
 whatis( "Name: OPENBLAS" )
 whatis( "Version: "..version )
 
-local openblas_prefix  = libs_dir.."/openblas/"..version
+local openblas_prefix  = libs_dir.."/openblas/"..version.."/"..compiler.."/"..compiler_version
 
 if isDir(openblas_prefix) then
 else LmodError("module reports "..openblas_prefix.." is not a directory! Module not loaded.")
@@ -28,6 +33,9 @@ setenv( "OPENBLAS_BIN", pathJoin(openblas_prefix, "bin" ) )
 setenv( "OPENBLAS_LIB", pathJoin(openblas_prefix, "lib" ) )
 setenv( "BLAS_IMPLEMENTATION", "openblas" )
 setenv( "BLAS_VERSION", version )
+setenv( "BLAS_DIR", openblas_prefix )
+setenv( "BLAS_LIBS", pathJoin(openblas_prefix, "lib" ).."/libopenblas.so")
+setenv( "LAPACK_LIBS", pathJoin(openblas_prefix, "lib" ).."/libopenblas.so")
 
 -- Default to 1 thread
 setenv( "OPENBLAS_NUM_THREADS", 1 )
