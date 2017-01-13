69a7c58083849d766f9d5800293faf4675c8c2e7
Paul T. Bauman <ptbauman@gmail.com>
2017-01-13 17:53:50 -0500

    VTK 7.1 doesn't need the build patch

--- build_scripts/build_vtk.sh
+++ build_scripts/build_vtk.sh
@@ -29,13 +29,13 @@ cd $BUILDDIR
 tar -xzf $UBCESLAB_SWENV_PREFIX/sourcesdir/vtk/v$VTK_VERSION.tar.gz
 
 # For GCC >= 6.X.Y, VTK 6.3.0 and 7.0.0 have a bug in their build system
-export COMPILER_MAJOR_VERSION=`echo ${COMPILER_VERSION:?undefined} | cut -d . -f 1`
-echo $COMPILER_MAJOR_VERSION
-if [ "$COMPILER_MAJOR_VERSION" -gt 5 ]; then
-  cd VTK-$VTK_VERSION
-  patch -p0 < $CURRENT_DIR/vtk.patch
-  cd ..
-fi
+#export COMPILER_MAJOR_VERSION=`echo ${COMPILER_VERSION:?undefined} | cut -d . -f 1`
+#echo $COMPILER_MAJOR_VERSION
+#if [ "$COMPILER_MAJOR_VERSION" -gt 5 ]; then
+#  cd VTK-$VTK_VERSION
+#  patch -p0 < $CURRENT_DIR/vtk.patch
+#  cd ..
+#fi
 
 # CMake builds *really* don't like in-source builds
 mkdir -p build
@@ -45,6 +45,7 @@ cd build
 -D BUILD_SHARED_LIBS:BOOL=ON \
 -D CMAKE_INSTALL_PREFIX:PATH=$VTK_DIR \
 -D CMAKE_BUILD_TYPE:STRING=Release \
+-D VTK_Group_MPI:BOOL=ON \
 $BUILDDIR/VTK-$VTK_VERSION 2>&1 && touch build_cmd_success) | tee configure.log
 rm build_cmd_success
 (make -j ${NPROC:-1} all 2>&1 && touch build_cmd_success) | tee make.log
@@ -67,5 +68,6 @@ echo "local version = \"$VTK_VERSION\"" > $MODULEDIR/$VTK_VERSION.lua
 echo "local libs_dir = \"$UBCESLAB_SWENV_PREFIX/libs\"" >> $MODULEDIR/$VTK_VERSION.lua
 echo "local version_major = \"$VTK_MAJOR\"" >> $MODULEDIR/$VTK_VERSION.lua
 echo "local version_minor = \"$VTK_MINOR\"" >> $MODULEDIR/$VTK_VERSION.lua
+echo "local name = \"vtk\"" >> $MODULEDIR/$VTK_VERSION.lua
 
 cat ../modulefiles/vtk.lua >> $MODULEDIR/$VTK_VERSION.lua
